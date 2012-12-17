40e485a8c5653b1b68f03ce02e132638bff2287d
Cai Yufei <0.yfcai@gmail.com>
2012-12-17 13:15:40 +0100

    Markdown template

---
+++ exercises/I.scala
@@ -0,0 +1,26 @@
+Programming Languages and Types
+
+Exercise session I on 17.12.2012
+
+Hand in
+
+- anytime
+- as a single .scala file to pllecture@informatik
+
+Solution will be posted before 14.01.13, 1400 hrs.
+
+The tutor shall give feedback to each submission, without giving
+points. Homework does not count toward course requirement. Do them for
+your own benefit.
+
+While everyone should write their own solution, it is recommended to
+work face-to-face in small groups.
+
+
+I. Modular Semantics
+====================
+
+Copy
+[20-normalization.scala]
+(https://github.com/klauso/PLT2012/blob/master/lecturenotes/20-normalization.scala).
+
